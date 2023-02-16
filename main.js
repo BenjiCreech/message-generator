6ade5f75dc2f6c99f8220f4ad189e9dedc149d23
Benjamin Creech <ben@Benjamins-Mac-Studio.local>
2023-02-15 22:43:31 -0500

    oh my goodness

--- main.js
+++ main.js
@@ -4,7 +4,6 @@ let returnStatement = "testing output";
 
 
 //Outputs the selected quote to UI
-
 function deliverOutput() {
     document.getElementById("output").innerHTML += (`<h3>${returnStatement}</h3>`);
 }

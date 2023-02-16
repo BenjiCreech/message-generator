e35d65faa7e65b137c7cfcc401514fd249c31f15
Benjamin Creech <ben@Benjamins-Mac-Studio.local>
2023-02-15 22:10:10 -0500

    update js 2

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ let returnStatement = "testing output";
 //Outputs the selected quote to UI
 const output = document.createElement("h3");
 const node = document.createTextNode(returnStatement);
-output.appendChild(node);
+
 
 const element = document.getElementById("output");
-element.appendChild(output);
+element.appendChild(output.appendChild(node));

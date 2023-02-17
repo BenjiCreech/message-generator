f22800a32a1eb1bdc3c90854b97afb0b245989d7
Benjamin Creech <ben@Benjamins-Mac-Studio.local>
2023-02-16 22:00:45 -0500

    add character selection

--- main.js
+++ main.js
@@ -1,6 +1,50 @@
 
-let returnStatement = "<h3>" + "testing output" + "</h3>";
+//Set intital Variables
+let output = characterToQuote;
+let returnStatement = "<h3>" + output + "</h3>";
 
+let characterToQuote = selectCharacter();
+
+//Randomly selects character to quote
+function selectCharacter() {
+    const randomNum = Math.floor(Math.random() * 6);
+    const charArray = ["Schmidt", "Nick", "Jess", "Cece", "Winston", "Coach"];
+    return charArray[randomNum];
+}
+
+//Random quote from selected character
+function selectQuote(name) {
+    const randomNum = Math.floor(Math.random() * 10);
+    const schmidtQuotes = [];
+    const nickQuotes = [];
+    const jessQuotes = [];
+    const ceceQuotes = [];
+    const winstonQuotes = [];
+    const coachQuotes = [];
+
+    switch(name) {
+        case 'Schmidt':
+            return schmidtQuotes[randomNum];
+            break;
+        case 'Nick':
+            return nickQuotes[randomNum];
+            break;
+        case 'Jess':
+            return jessQuotes[randomNum];
+            break;
+        case 'Cece':
+            return ceceQuotes[randomNum];
+            break;
+        case 'Winston':
+            return winstonQuotes[randomNum];
+            break;
+        case 'Coach':
+            return coachQuotes[randomNum];
+            break;
+        default:
+            break;
+    }
+}
 
 
 //Outputs the selected quote to UI

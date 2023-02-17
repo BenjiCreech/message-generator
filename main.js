31febc99ef82ba9cc5fb2b68085132d8ea44513a
Benjamin Creech <ben@Benjamins-Mac-Studio.local>
2023-02-16 22:47:01 -0500

    another edit

--- main.js
+++ main.js
@@ -50,7 +50,7 @@ function selectQuote(name) {
 
 //Outputs the selected quote to UI
 function deliverOutput() {
-    document.getElementById("output").insertAdjacentElement += (returnStatement);
+    document.getElementById("output").insertAdjacentElement('beforeend', returnStatement);
 }
 
 //Event Listener
